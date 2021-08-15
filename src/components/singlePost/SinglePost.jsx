19a34b7c3c5f6f5ae5c00f793db62c4b9f5f05d4
EpatelKowshik <88603935+EpatelKowshik@users.noreply.github.com>
2021-08-15 23:37:52 +0530

    hopefully last url change

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -9,7 +9,7 @@ export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "image/";
+  const PF = "https://epk-blogpost.herokuapp.com/image/";
   const { user } = useContext(Context);
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
